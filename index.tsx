664b8aecedea86a41f6aada68f2c3cfa7fcb4d4b
samproductions <samproductionns@gmail.com>
2026-01-05 20:17:04 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,14 +1,17 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// Esta linha abaixo é essencial para carregar o design/cores do site
+import './index.css'; 
 
 const rootElement = document.getElementById('root');
+
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
