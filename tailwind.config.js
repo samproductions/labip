86ecad837cd555dce2b3158ce93c26f5705a93fa
samproductions <samproductionns@gmail.com>
2026-01-05 20:21:05 -0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./index.tsx",
+    "./App.tsx",
+    "./componentes/**/*.{js,ts,jsx,tsx}",
+    "./serviços/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
